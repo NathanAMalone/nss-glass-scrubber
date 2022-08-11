f7d1f71c40c19edba494fab26df9322e62608423
Nathan Malone <nathanamalone@gmail.com>
2022-08-11 14:21:21 -0500

    Glass Scrubber complete

---
+++ main.js
@@ -0,0 +1,22 @@
+const dishes = [
+    "Dinner plate", "Water glass", "Salad bowl",
+    "Dinner plate", "Wine glass", "Spoon",
+    "Spoon", "Fork", "Salad bowl", "Whiskey glass",
+    "Fork", "Spoon", "Fork"
+]
+
+/*
+    Declare three variables to store the string value
+    of a glass in the array. Use the correct index to
+    get the right value.
+*/
+const glassWater = dishes[1]
+const glassWine = dishes[4]
+const glassWhisk = dishes[9]
+
+console.log("I am cleaning the following glasses:")
+
+// Put each variable in one of the parenthesis below
+console.log(glassWater)
+console.log(glassWine)
+console.log(glassWhisk)
